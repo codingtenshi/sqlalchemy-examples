903238c00ddf32bc4fae05576582cca3b06a7f30
kireitensh <karina.gornicka@wp.pl>
2023-05-13 14:35:02 +0200

    first version

--- application.py
+++ application.py
@@ -1,12 +1,31 @@
 from sqlalchemy import create_engine
 
 
-def main():
+def main():  # pobieranie tabeli
+
     engine = create_engine(
         "mysql+pymysql://root:root@localhost:3306/company"
     )
-    print(engine.table_names())
+    sql = 'SELECT * FROM archived_users;'
+
+    with engine.connect() as conn: # działamy cały czas z tym samym połączeniem
+        result = conn.execute(sql)
+        for row in result:
+            print(row)
+
+    #result = engine.execute(sql) # wszystkie wiersze
+    #for row in result: # pobieram wszystkie maile z bd
+        #print(row.email)
+    #result.fetchone()
+    rows = result.fetchall()
+        #for row in rows:  #  for row in result:
+        #print(row)     # print(row)
+    #  print(result)
+
+    #  item = result.fetchone()
+    #  print(item.email) # typ RowProxy
 
 
 if __name__ == '__main__':
     main()
+
